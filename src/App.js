7c3948ef9b56af3d02b4ab2599494c39ebdb3018
Sahil Athia <sahilathia918@gmail.com>
2021-03-30 13:59:55 -0400

    load data with axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'; 
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API}&q=London&aqi=no`)
+      .then(response => console.log(response))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
