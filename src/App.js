de58b88201b9b723c4a32013e88933b4f55a5e4b
Sahil Athia <sahilathia918@gmail.com>
2021-04-03 17:40:28 -0400

    add location form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react'
+import { Form, Button } from 'react-bootstrap';
 import axios from 'axios'; 
 
 function App() {
   const [data, setData] = useState(false)
+  const [location, setLocation] = useState("")
   useEffect(() => {
     axios
       .get(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API}&q=London&aqi=no`)
@@ -13,6 +15,19 @@ function App() {
 
   return (
     <div className="App">
+      <Form onSubmit={handleSubmit} >
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Select Your Location</Form.Label>
+          <Form.Control 
+            placeholder="Location:"
+            type="text"
+            name="location"
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+          />
+        </Form.Group>
+      </Form>
+  
       {data && <div>{data.location.name}</div>}
     </div>
   );
